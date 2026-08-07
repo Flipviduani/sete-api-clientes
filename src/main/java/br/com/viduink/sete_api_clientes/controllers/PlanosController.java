f612980493345e86a0e4852ebbbda5556dd3f4fa
Flipviduani <flipviduani@hotmail.com>
2026-08-07 12:40:09 -0300

    Added REST API frameworks.

--- src/main/java/br/com/viduink/sete_api_clientes/controllers/PlanosController.java
+++ src/main/java/br/com/viduink/sete_api_clientes/controllers/PlanosController.java
@@ -1,4 +1,15 @@
 package br.com.viduink.sete_api_clientes.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/planos")
 public class PlanosController {
+
+    @GetMapping
+    public String get(){
+        return "Consulta realizada com sucesso!";
+    }
 }
