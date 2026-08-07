22de345d8aecbc6efa233741ce997b3ce334840f
Flipviduani <flipviduani@hotmail.com>
2026-08-07 10:39:45 -0300

    First commit

---
+++ src/main/java/br/com/viduink/sete_api_clientes/SeteApiClientesApplication.java
@@ -0,0 +1,13 @@
+package br.com.viduink.sete_api_clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeteApiClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeteApiClientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/viduink/sete_api_clientes/SeteApiClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.viduink.sete_api_clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeteApiClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
